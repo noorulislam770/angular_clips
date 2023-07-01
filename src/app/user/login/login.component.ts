6b37301329c3c169c1b275b155e5048a000f05e4
Noor Ul Islam <noorulislam770@gmail.com>
2023-07-01 17:44:31 +0500

    Done with the Master Project TEmplate Forms Section

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  credentials = {
+    email : '',
+    password: ''
+  }
+
+
+
+  login(){
+    console.log(this.credentials)
+  }
 
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -36,7 +36,7 @@ export class RegisterComponent {
     ]),
     password: new FormControl('',[
       Validators.required,
-      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}/gm)
     ]),
     confirm_password: new FormControl('',[
       Validators.required
