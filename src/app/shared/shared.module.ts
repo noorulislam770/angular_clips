1d63cf3cb4e21dced95e3b2e80b3c0d6edfe8fb2
Noor Ul Islam <noorulislam770@gmail.com>
2023-07-01 16:21:26 +0500

    Done with reactive forms section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,38 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective,IConfig, provideEnvironmentNgxMask, provideNgxMask  } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component'
 
-
-
+export const MASK_CONFIG = new InjectionToken<IConfig>('ngx-mask config');
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+
   ],
   exports : [
     ModalComponent,
     TabComponent,
     TabsContainerComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  providers : [
+    provideNgxMask(),
+    provideEnvironmentNgxMask()
   ]
 })
 export class SharedModule { }
